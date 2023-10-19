fae3a283977000d6000b6c73b80a41287745f0c6
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-19 14:18:51 +0430

    Add flash cart message

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
     def index
-        
+        flash.now[:notice] = "Logged in successfully"
+        flash.now[:alert] = "Invalid email address"
     end
 end
